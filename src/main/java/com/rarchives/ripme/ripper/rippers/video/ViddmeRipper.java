568918877d2a1e41abb95ad2f4bd32c6eb2f203f
4pr0n <admin@rarchives.com>
2014-06-12 22:54:06 -0700

    1.0.65 - Vidd.me video ripper

---
+++ src/main/java/com/rarchives/ripme/ripper/rippers/video/ViddmeRipper.java
@@ -0,0 +1,70 @@
+package com.rarchives.ripme.ripper.rippers.video;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import com.rarchives.ripme.ripper.VideoRipper;
+
+public class ViddmeRipper extends VideoRipper {
+
+    private static final String HOST = "vidd";
+
+    public ViddmeRipper(URL url) throws IOException {
+        super(url);
+    }
+
+    @Override
+    public String getHost() {
+        return HOST;
+    }
+
+    @Override
+    public boolean canRip(URL url) {
+        Pattern p = Pattern.compile("^https?://[wm.]*vidd\\.me/[a-zA-Z0-9]+.*$");
+        Matcher m = p.matcher(url.toExternalForm());
+        return m.matches();
+    }
+    
+    @Override
+    public URL sanitizeURL(URL url) throws MalformedURLException {
+        return url;
+    }
+
+    @Override
+    public String getGID(URL url) throws MalformedURLException {
+        Pattern p = Pattern.compile("^https?://[wm.]*vidd\\.me/([a-zA-Z0-9]+).*$");
+        Matcher m = p.matcher(url.toExternalForm());
+        if (m.matches()) {
+            return m.group(1);
+        }
+
+        throw new MalformedURLException(
+                "Expected vidd.me format:"
+                        + "vidd.me/id"
+                        + " Got: " + url);
+    }
+
+    @Override
+    public void rip() throws IOException {
+        logger.info("    Retrieving " + this.url.toExternalForm());
+        Document doc = Jsoup.connect(this.url.toExternalForm())
+                            .userAgent(USER_AGENT)
+                            .get();
+        Elements videos = doc.select("meta[name=twitter:player:stream]");
+        if (videos.size() == 0) {
+            throw new IOException("Could not find twitter:player:stream at " + url);
+        }
+        String vidUrl = videos.first().attr("content");
+        vidUrl = vidUrl.replaceAll("&amp;", "&");
+        addURLToDownload(new URL(vidUrl), HOST + "_" + getGID(this.url));
+        waitForThreads();
+    }
+}
