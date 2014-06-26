5afbc1ce3ac5b77cce6c7ff77c0172f23341ef77
4pr0n <admin@rarchives.com>
2014-06-26 00:48:15 -0700

    1.0.73 - Datw.in ripper for #8

---
+++ src/main/java/com/rarchives/ripme/ripper/rippers/DatwinRipper.java
@@ -0,0 +1,65 @@
+package com.rarchives.ripme.ripper.rippers;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import com.rarchives.ripme.ripper.AbstractHTMLRipper;
+import com.rarchives.ripme.utils.Http;
+
+public class DatwinRipper extends AbstractHTMLRipper {
+
+    public DatwinRipper(URL url) throws IOException {
+        super(url);
+    }
+
+    @Override
+    public String getHost() {
+        return "datwin";
+    }
+    @Override
+    public String getDomain() {
+        return "datw.in";
+    }
+
+    @Override
+    public String getGID(URL url) throws MalformedURLException {
+        Pattern p = Pattern.compile("^.*datw.in/([a-zA-Z0-9\\-_]+).*$");
+        Matcher m = p.matcher(url.toExternalForm());
+        if (m.matches()) {
+            return m.group(1);
+        }
+        throw new MalformedURLException(
+                "Expected datw.in gallery formats: "
+                        + "datw.in/..."
+                        + " Got: " + url);
+    }
+
+    @Override
+    public Document getFirstPage() throws IOException {
+        return Http.url(url).get();
+    }
+    
+    @Override
+    public List<String> getURLsFromPage(Document doc) {
+        List<String> imageURLs = new ArrayList<String>();
+        for (Element thumb : doc.select("img.attachment-thumbnail")) {
+            String image = thumb.attr("src");
+            image = image.replaceAll("-\\d{1,3}x\\d{1,3}", "");
+            imageURLs.add(image);
+        }
+        return imageURLs;
+    }
+    
+    @Override
+    public void downloadURL(URL url, int index) {
+        addURLToDownload(url, getPrefix(index));
+    }
+}
