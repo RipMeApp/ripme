80563991b1d84abce0d73c5f7071b25e99f3d03d
Erwin de Haan <git@erayan.eu>
2014-09-08 00:39:51 +0200

    Removed default netbeans license/author block.

--- src/main/java/com/rarchives/ripme/ripper/rippers/ripperhelpers/ChanSite.java
+++ src/main/java/com/rarchives/ripme/ripper/rippers/ripperhelpers/ChanSite.java
@@ -1,17 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package com.rarchives.ripme.ripper.rippers.ripperhelpers;
 
 import java.util.List;
 
-/**
- *
- * @author Erwin
- */
 public class ChanSite {
     //The domains where the threads are hosted.
     public List<String> domains;
