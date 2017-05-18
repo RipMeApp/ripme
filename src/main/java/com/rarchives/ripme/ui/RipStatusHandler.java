44fe9885abbd6d20328a811714c2cd2425fba6e7
Gaboso <gabrielsantiago2@gmail.com>
2017-05-18 01:44:51 -0400

    Removed public modifier for methods on interfaces because it is redundant.

--- src/main/java/com/rarchives/ripme/ui/RipStatusHandler.java
+++ src/main/java/com/rarchives/ripme/ui/RipStatusHandler.java
@@ -1,14 +1,12 @@
-
 package com.rarchives.ripme.ui;
 
 import com.rarchives.ripme.ripper.AbstractRipper;
 
 /**
- *
  * @author Mads
  */
 public interface RipStatusHandler {
 
-    public void update(AbstractRipper ripper, RipStatusMessage message);
+    void update(AbstractRipper ripper, RipStatusMessage message);
 
 }
