4e3c50a0dca48de0c030c44e5fabcfc5831108de
metaprime <metaprime@users.noreply.github.com>
2017-11-21 04:19:04 -0800

    Removed FapprovedRipper (permanently down)

--- src/main/java/com/rarchives/ripme/utils/RipUtils.java
+++ src/main/java/com/rarchives/ripme/utils/RipUtils.java
@@ -157,7 +157,6 @@ public class RipUtils {
         if (url == null) url = urlFromSiteDirectoryName(dir, "bfcakes",     "http://www.bcfakes.com/celebritylist/", "");
         if (url == null) url = urlFromSiteDirectoryName(dir, "drawcrowd",   "http://drawcrowd.com/", "");
         if (url == null) url = urlFromSiteDirectoryName(dir.replace("-", "/"), "ehentai", "http://g.e-hentai.org/g/", "");
-        if (url == null) url = urlFromSiteDirectoryName(dir, "fapproved", "http://fapproved.com/users/", "");
         if (url == null) url = urlFromSiteDirectoryName(dir, "vinebox", "http://finebox.co/u/", "");
         if (url == null) url = urlFromSiteDirectoryName(dir, "imgbox", "http://imgbox.com/g/", "");
         if (url == null) url = urlFromSiteDirectoryName(dir, "modelmayhem", "http://www.modelmayhem.com/", "");
