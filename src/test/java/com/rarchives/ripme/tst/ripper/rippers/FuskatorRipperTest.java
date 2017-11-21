585cf8617d5bc7f05e10c6a5fcebce20d1e3ad39
metaprime <metaprime@users.noreply.github.com>
2017-11-21 02:33:23 -0800

    Re-enable FuskatorRipperTest since there doesn't seem to be anything wrong. (#244)

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/FuskatorRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/FuskatorRipperTest.java
@@ -6,10 +6,8 @@ import java.net.URL;
 import com.rarchives.ripme.ripper.rippers.FuskatorRipper;
 
 public class FuskatorRipperTest extends RippersTest {
-    /*
     public void testFuskatorAlbum() throws IOException {
         FuskatorRipper ripper = new FuskatorRipper(new URL("http://fuskator.com/full/emJa1U6cqbi/index.html"));
         testRipper(ripper);
     }
-    */
 }
