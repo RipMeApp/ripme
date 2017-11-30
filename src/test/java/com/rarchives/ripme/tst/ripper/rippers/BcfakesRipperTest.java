259ce0690b67bb4699ce305e2bb29f7bc39c2cff
metaprime <metaprime@users.noreply.github.com>
2017-11-30 00:27:30 -0800

    Fix typo in test name of new tests.

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/BcfakesRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/BcfakesRipperTest.java
@@ -6,7 +6,7 @@ import java.net.URL;
 import com.rarchives.ripme.ripper.rippers.BcfakesRipper;
 
 public class BcfakesRipperTest extends RippersTest {
-    public void testVidbleRip() throws IOException {
+    public void testRip() throws IOException {
         BcfakesRipper ripper = new BcfakesRipper(new URL("http://www.bcfakes.com/celebritylist/olivia-wilde/"));
         testRipper(ripper);
     }
