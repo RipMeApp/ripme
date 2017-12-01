7dc537a471236ec67ecad5be0cf08581a851e536
cyian-1756 <devnull64@vfemail.net>
2017-11-30 21:57:20 -0500

    Added oglaf unit test

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/OglafRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/OglafRipperTest.java
@@ -1,4 +1,13 @@
 package com.rarchives.ripme.tst.ripper.rippers;
 
-public class OglafRipperTest {
+import java.io.IOException;
+import java.net.URL;
+
+import com.rarchives.ripme.ripper.rippers.OglafRipper;
+
+public class OglafRipperTest extends RippersTest {
+    public void testRip() throws IOException {
+        OglafRipper ripper = new OglafRipper(new URL("http://oglaf.com/plumes/"));
+        testRipper(ripper);
+    }
 }
