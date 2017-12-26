468a46505b662e310e97fbe037c44ef935f32e1d
Kevin Jiang <>
2017-12-26 17:51:15 -0500

    Gfycat /gifs/detail links now working.
    JUnit Test Case. Tests both normal URLs and extended URLs.
    Task #356 - Gfycat url variant not parsing

---
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/GfycatRipperTest.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rarchives.ripme.tst.ripper.rippers;
+
+
+import java.io.IOException;
+import java.net.URL;
+
+import com.rarchives.ripme.ripper.rippers.video.GfycatRipper;
+
+/**
+ *
+ * @author Bob Zhang
+ */
+public class GfycatRipperTest extends RippersTest{
+    
+
+    /**
+     * What normal people would try to rip.
+     * @throws IOException 
+     */
+    public void testGfycatNormal() throws IOException {
+        GfycatRipper ripper = new GfycatRipper(new URL("https://gfycat.com/TemptingExcellentIchthyosaurs"));
+        testRipper(ripper);
+    }
+    
+     /**
+     * For the kind of people who insist on putting "/gifs/detail/" in the URL
+     * @throws IOException 
+     */
+    public void testGfycatExtendedURL() throws IOException {
+        GfycatRipper ripper = new GfycatRipper(new URL("https://gfycat.com/gifs/detail/TemptingExcellentIchthyosaurs"));
+        testRipper(ripper);
+    }
+}
+
