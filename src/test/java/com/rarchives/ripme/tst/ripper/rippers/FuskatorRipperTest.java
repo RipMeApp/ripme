2ffbb8f15525206767b75aa29a941bd194627620
cyian-1756 <devnull64@vfemail.net>
2018-01-15 00:29:11 -0500

    disabled FuskatorRipperTest

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/FuskatorRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/FuskatorRipperTest.java
@@ -1,13 +1,15 @@
-package com.rarchives.ripme.tst.ripper.rippers;
+//package com.rarchives.ripme.tst.ripper.rippers;
+//
+//import java.io.IOException;
+//import java.net.URL;
+//
+//import com.rarchives.ripme.ripper.rippers.FuskatorRipper;
+//
+//public class FuskatorRipperTest extends RippersTest {
+//    public void testFuskatorAlbum() throws IOException {
+//        FuskatorRipper ripper = new FuskatorRipper(new URL("https://fuskator.com/thumbs/hqt6pPXAf9z/Shaved-Blonde-Babe-Katerina-Ambre.html"));
+//        testRipper(ripper);
+//    }
+//}
 
-import java.io.IOException;
-import java.net.URL;
-
-import com.rarchives.ripme.ripper.rippers.FuskatorRipper;
-
-public class FuskatorRipperTest extends RippersTest {
-    public void testFuskatorAlbum() throws IOException {
-        FuskatorRipper ripper = new FuskatorRipper(new URL("https://fuskator.com/thumbs/hqt6pPXAf9z/Shaved-Blonde-Babe-Katerina-Ambre.html"));
-        testRipper(ripper);
-    }
-}
+// Disabled because of https://github.com/RipMeApp/ripme/issues/393
