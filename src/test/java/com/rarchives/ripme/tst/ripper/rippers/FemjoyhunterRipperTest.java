d80488dc805d8650f0576d454669b90f6e027f90
cyian-1756 <devnull64@vfemail.net>
2018-03-11 22:42:07 -0400

    Fixed test

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/FemjoyhunterRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/FemjoyhunterRipperTest.java
@@ -5,7 +5,7 @@ import java.net.URL;
 
 import com.rarchives.ripme.ripper.rippers.FemjoyhunterRipper;
 
-public class FemjoyRipperTest extends RippersTest {
+public class FemjoyhunterRipperTest extends RippersTest {
     public void testRip() throws IOException {
         FemjoyhunterRipper ripper = new FemjoyhunterRipper(new URL("https://www.femjoyhunter.com/alisa-i-got-nice-big-breasts-and-fine-ass-so-she-seems-to-be-a-hottest-brunette-5936/"));
         testRipper(ripper);
