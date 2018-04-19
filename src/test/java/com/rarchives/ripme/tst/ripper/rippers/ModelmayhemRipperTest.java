dcad5c325ec8e25bed9bbdd79747775d5a3e66bb
cyian-1756 <devnull64@vfemail.net>
2018-04-19 19:16:15 -0400

    Extended some unit tests to include getGid

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/ModelmayhemRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/ModelmayhemRipperTest.java
@@ -10,4 +10,9 @@ public class ModelmayhemRipperTest extends RippersTest {
         ModelmayhemRipper ripper = new ModelmayhemRipper(new URL("https://www.modelmayhem.com/portfolio/520206/viewall"));
         testRipper(ripper);
     }
+
+    public void testGetGID() throws IOException {
+        ModelmayhemRipper ripper = new ModelmayhemRipper(new URL("https://www.modelmayhem.com/portfolio/520206/viewall"));
+        assertEquals("520206", ripper.getGID(new URL("https://www.modelmayhem.com/portfolio/520206/viewall")));
+    }
 }
