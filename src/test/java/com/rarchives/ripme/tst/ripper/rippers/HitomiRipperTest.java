e986194756c690a6e4c931901789a427565714f5
cyian-1756 <devnull64@vfemail.net>
2018-04-19 18:58:47 -0400

    Extended HitomiRipper unit test

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/HitomiRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/HitomiRipperTest.java
@@ -9,5 +9,6 @@ public class HitomiRipperTest extends RippersTest {
     public void testRip() throws IOException {
         HitomiRipper ripper = new HitomiRipper(new URL("https://hitomi.la/galleries/975973.html"));
         testRipper(ripper);
+        assertTrue(ripper.getGID(new URL("https://hitomi.la/galleries/975973.html")).equals("975973"));
     }
 }
