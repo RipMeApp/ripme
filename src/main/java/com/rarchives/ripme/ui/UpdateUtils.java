19e4a95c964a7dd0ac3cd42d50d72aa55c0083f8
cyian-1756 <devnull64@vfemail.net>
2018-05-10 07:30:05 -0400

    1.7.44: Fixed instagram ripper regex

--- src/main/java/com/rarchives/ripme/ui/UpdateUtils.java
+++ src/main/java/com/rarchives/ripme/ui/UpdateUtils.java
@@ -21,7 +21,7 @@ import com.rarchives.ripme.utils.Utils;
 public class UpdateUtils {
 
     private static final Logger logger = Logger.getLogger(UpdateUtils.class);
-    private static final String DEFAULT_VERSION = "1.7.43";
+    private static final String DEFAULT_VERSION = "1.7.44";
     private static final String REPO_NAME = "ripmeapp/ripme";
     private static final String updateJsonURL = "https://raw.githubusercontent.com/" + REPO_NAME + "/master/ripme.json";
     private static final String mainFileName = "ripme.jar";
