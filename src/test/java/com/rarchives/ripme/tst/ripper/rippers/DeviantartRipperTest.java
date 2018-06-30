e8df04a01d2534df97a9ff27fc40b04c46b5ad56
cyian-1756 <devnull64@vfemail.net>
2018-06-29 23:04:13 -0400

    Fixed DeviantartRipper unit tests

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/DeviantartRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/DeviantartRipperTest.java
@@ -7,18 +7,18 @@ import com.rarchives.ripme.ripper.rippers.DeviantartRipper;
 
 public class DeviantartRipperTest extends RippersTest {
     public void testDeviantartAlbum() throws IOException {
-        DeviantartRipper ripper = new DeviantartRipper(new URL("http://airgee.deviantart.com/gallery/"));
+        DeviantartRipper ripper = new DeviantartRipper(new URL("https://www.deviantart.com/airgee/gallery/"));
         testRipper(ripper);
     }
 
     public void testDeviantartNSFWAlbum() throws IOException {
         // NSFW gallery
-        DeviantartRipper ripper = new DeviantartRipper(new URL("http://faterkcx.deviantart.com/gallery/"));
+        DeviantartRipper ripper = new DeviantartRipper(new URL("https://www.deviantart.com/faterkcx/gallery/"));
         testRipper(ripper);
     }
 
     public void testGetGID() throws IOException {
-        URL url = new URL("http://airgee.deviantart.com/gallery/");
+        URL url = new URL("https://www.deviantart.com/airgee/gallery/");
         DeviantartRipper ripper = new DeviantartRipper(url);
         assertEquals("airgee", ripper.getGID(url));
     }
