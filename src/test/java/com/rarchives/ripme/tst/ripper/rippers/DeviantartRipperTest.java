39ee96d49cea41a15a39e630f636c0967f82623b
cyian-1756 <devnull64@vfemail.net>
2018-07-01 19:25:32 -0400

    Added better url sanitization for DeviantartRipper and some unit tests

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/DeviantartRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/DeviantartRipperTest.java
@@ -2,6 +2,8 @@ package com.rarchives.ripme.tst.ripper.rippers;
 
 import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 import com.rarchives.ripme.ripper.rippers.DeviantartRipper;
 import com.rarchives.ripme.utils.Http;
@@ -31,5 +33,17 @@ public class DeviantartRipperTest extends RippersTest {
         Document doc = Http.url(url).get();
         assertEquals("airgee", ripper.getUsername(doc));
         assertEquals("714589", ripper.getGalleryID(doc));
+    }
+
+    public void testSanitizeURL() throws IOException {
+        List<URL> urls = new ArrayList<URL>();
+        urls.add(new URL("https://www.deviantart.com/airgee/"));
+        urls.add(new URL("https://www.deviantart.com/airgee"));
+        urls.add(new URL("https://www.deviantart.com/airgee/gallery/"));
+
+        for (URL url : urls) {
+            DeviantartRipper ripper = new DeviantartRipper(url);
+            assertEquals("https://www.deviantart.com/airgee/gallery/", ripper.sanitizeURL(url).toExternalForm());
+        }
     }
 }
