ae6d09c6e85578c301f789b7c7e113a5204cf0fc
cyian-1756 <devnull64@vfemail.net>
2018-07-01 15:14:09 -0400

    Added some unit tests for DeviantartRipper

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/DeviantartRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/DeviantartRipperTest.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.net.URL;
 
 import com.rarchives.ripme.ripper.rippers.DeviantartRipper;
+import com.rarchives.ripme.utils.Http;
+import org.jsoup.nodes.Document;
 
 public class DeviantartRipperTest extends RippersTest {
     public void testDeviantartAlbum() throws IOException {
@@ -21,5 +23,13 @@ public class DeviantartRipperTest extends RippersTest {
         URL url = new URL("https://www.deviantart.com/airgee/gallery/");
         DeviantartRipper ripper = new DeviantartRipper(url);
         assertEquals("airgee", ripper.getGID(url));
+    }
+
+    public void testGetGalleryIDAndUsername() throws IOException {
+        URL url = new URL("https://www.deviantart.com/airgee/gallery/");
+        DeviantartRipper ripper = new DeviantartRipper(url);
+        Document doc = Http.url(url).get();
+        assertEquals("airgee", ripper.getUsername(doc));
+        assertEquals("714589", ripper.getGalleryID(doc));
     }
 }
