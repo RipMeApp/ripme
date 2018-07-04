6d0a6fad864a0de6d394332ba892636bb6ef5499
cyian-1756 <devnull64@vfemail.net>
2018-07-04 00:36:36 -0400

    1.7.57: Got DeviantartRipper working again; Imagefap ripper now downloads full sized images; Twitter ripper can now rip extended tweets; Added nl_NL translation

--- src/main/java/com/rarchives/ripme/ui/UpdateUtils.java
+++ src/main/java/com/rarchives/ripme/ui/UpdateUtils.java
@@ -20,7 +20,7 @@ import com.rarchives.ripme.utils.Utils;
 public class UpdateUtils {
 
     private static final Logger logger = Logger.getLogger(UpdateUtils.class);
-    private static final String DEFAULT_VERSION = "1.7.56";
+    private static final String DEFAULT_VERSION = "1.7.57";
     private static final String REPO_NAME = "ripmeapp/ripme";
     private static final String updateJsonURL = "https://raw.githubusercontent.com/" + REPO_NAME + "/master/ripme.json";
     private static final String mainFileName = "ripme.jar";
