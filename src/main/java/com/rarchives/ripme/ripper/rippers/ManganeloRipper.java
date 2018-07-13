a65ad6f75616b8f022a5dee0fd773353248e1783
cyian-1756 <devnull64@vfemail.net>
2018-07-13 18:14:36 -0400

    Fixed ManganeloRipper

--- src/main/java/com/rarchives/ripme/ripper/rippers/ManganeloRipper.java
+++ src/main/java/com/rarchives/ripme/ripper/rippers/ManganeloRipper.java
@@ -99,7 +99,7 @@ public class ManganeloRipper extends AbstractHTMLRipper {
             Collections.reverse(urlsToGrab);
 
             for (String url : urlsToGrab) {
-                result.addAll(getURLsFromChap("https:" + url));
+                result.addAll(getURLsFromChap(url));
             }
         } else if (url.toExternalForm().contains("/chapter/")) {
             result.addAll(getURLsFromChap(doc));
