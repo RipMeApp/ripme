509baee6f320979513c1d2d838ca188613330a87
cyian-1756 <devnull64@vfemail.net>
2018-08-30 15:04:55 -0400

    Replaced motherless unittest

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/MotherlessRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/MotherlessRipperTest.java
@@ -8,7 +8,7 @@ import com.rarchives.ripme.ripper.rippers.MotherlessRipper;
 public class MotherlessRipperTest extends RippersTest {
     // https://github.com/RipMeApp/ripme/issues/238 - MotherlessRipperTest is flaky on Travis CI
     public void testMotherlessAlbumRip() throws IOException {
-        MotherlessRipper ripper = new MotherlessRipper(new URL("http://motherless.com/G1E5C971"));
+        MotherlessRipper ripper = new MotherlessRipper(new URL("https://motherless.com/G1168D90"));
         testRipper(ripper);
     }
 }
