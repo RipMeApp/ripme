d257794efd846d6143f98e43a173d8e47fef8ca0
metaprime <metaprime@users.noreply.github.com>
2018-09-11 01:19:34 -0700

    Added Test file stubs for rippers that did not have corresponding test files.
    See issues #915, #916, #917, #918, #919, #920, #921, #922, #923, #924, #925

---
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/HbrowseRipperTest.java
@@ -0,0 +1,10 @@
+package com.rarchives.ripme.tst.ripper.rippers;
+
+import java.io.IOException;
+import java.net.URL;
+
+import com.rarchives.ripme.ripper.rippers.HbrowseRipper;
+
+public class HbrowseRipperTest extends RippersTest {
+    // TODO add a test
+}
