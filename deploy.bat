98c81b3d70aaf6fd93f67c207402010b6f2500eb
digitalnoise <digitalnoise@glasken.com>
2018-09-12 17:00:54 -0500

    Revert "Customized deploy.bat for my personal uage."
    This reverts commit 9501e7407cad590e8b1adf53431cfcd0f49bc602.
    
    Moving to a untracked private file.

--- deploy.bat
+++ deploy.bat
@@ -1,3 +1,2 @@
 @echo off
 powershell -c ".\deploy.ps1 -source (Join-Path target (Get-Item -Path .\target\* -Filter *.jar)[0].Name) -dest ripme.jar"
-powershell -c ".\deploy.ps1 -source (Join-Path target (Get-Item -Path .\target\* -Filter *.jar)[0].Name) -dest C:\app\ripme.jar"
