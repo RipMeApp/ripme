db7707664c647ce22eb0bffc85cbf52ef745247f
digitalnoise <digitalnoise@glasken.com>
2018-09-16 14:03:20 -0500

    Fixed unit test.  Closes digitalnoise/ripme#2

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/RulePornRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/RulePornRipperTest.java
@@ -3,17 +3,17 @@ package com.rarchives.ripme.tst.ripper.rippers;
 import java.io.IOException;
 import java.net.URL;
 
-import com.rarchives.ripme.ripper.rippers.Tubex6Ripper;
+import com.rarchives.ripme.ripper.rippers.RulePornRipper;
 
 public class RulePornRipperTest extends RippersTest {
     public void testRip() throws IOException {
-        Tubex6Ripper ripper = new Tubex6Ripper(new URL("https://ruleporn.com/are-you-going-to-fill-my-lil-pussy-up/"));
+        RulePornRipper ripper = new RulePornRipper(new URL("https://ruleporn.com/are-you-going-to-fill-my-lil-pussy-up/"));
         testRipper(ripper);
     }
 
     public void testGetGID() throws IOException {
         URL url = new URL("https://ruleporn.com/are-you-going-to-fill-my-lil-pussy-up/");
-        Tubex6Ripper ripper = new Tubex6Ripper(url);
+        RulePornRipper ripper = new RulePornRipper(url);
         assertEquals("are-you-going-to-fill-my-lil-pussy-up", ripper.getGID(url));
     }
 }
