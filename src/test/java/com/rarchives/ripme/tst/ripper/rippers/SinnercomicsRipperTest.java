d352bf4f3b4da73c8b06d9eb8dd3bbc99d067d73
buzzlightmonth <44553885+buzzlightmonth@users.noreply.github.com>
2018-10-28 22:26:30 +0100

    Added pinups and homepage to Sinner Comics ripper

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/SinnercomicsRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/SinnercomicsRipperTest.java
@@ -7,13 +7,39 @@ import com.rarchives.ripme.ripper.rippers.SinnercomicsRipper;
 
 public class SinnercomicsRipperTest extends RippersTest {
     public void testSinnercomicsAlbum() throws IOException {
-        SinnercomicsRipper ripper = new SinnercomicsRipper(new URL("https://sinnercomics.com/comic/beyond-the-hotel-page-01/"));
+        SinnercomicsRipper ripper;
+
+        // Comic test
+        ripper = new SinnercomicsRipper(new URL("https://sinnercomics.com/comic/beyond-the-hotel-page-01/"));
         testRipper(ripper);
+
+        // Homepage test
+        ripper = new SinnercomicsRipper(new URL("https://sinnercomics.com/page/2/"));
+        testRipper(ripper);
+
+        // Pinup test
+        ripper = new SinnercomicsRipper(new URL("https://sinnercomics.com/elsa-frozen-2/#comments"));
+        testRipper(ripper);
+
     }
 
     public void testGetGID() throws IOException {
-        URL url = new URL("https://sinnercomics.com/comic/beyond-the-hotel-page-01/");
-        SinnercomicsRipper ripper = new SinnercomicsRipper(url);
+        URL url;
+        SinnercomicsRipper ripper;
+
+        // Comic test
+        url = new URL("https://sinnercomics.com/comic/beyond-the-hotel-page-01/");
+        ripper = new SinnercomicsRipper(url);
         assertEquals("beyond-the-hotel", ripper.getGID(url));
+
+        // Homepage test
+        url = new URL("https://sinnercomics.com/page/2/");
+        ripper = new SinnercomicsRipper(url);
+        assertEquals("homepage", ripper.getGID(url));
+
+        // Comic test
+        url = new URL("https://sinnercomics.com/elza-frozen-2/#comments");
+        ripper = new SinnercomicsRipper(url);
+        assertEquals("elza-frozen-2", ripper.getGID(url));
     }
 }
