42a662aca2a183398cb1b18008bce93ecdbb181e
Hüseyin Yağlı <hyagli@hotmail.com>
2018-10-30 04:26:08 +0300

    Madokami ripper

---
+++ src/main/java/com/rarchives/ripme/ripper/rippers/MadokamiRipper.java
@@ -0,0 +1,79 @@
+package com.rarchives.ripme.ripper.rippers;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import com.rarchives.ripme.ripper.AbstractHTMLRipper;
+import com.rarchives.ripme.utils.Base64;
+import com.rarchives.ripme.utils.Http;
+import com.rarchives.ripme.utils.Utils;
+
+public class MadokamiRipper extends AbstractHTMLRipper {
+    private static String API_KEY;
+    private Map<String, String> headers;
+
+    public MadokamiRipper(URL url) throws IOException {
+        super(url);
+        API_KEY = Utils.getConfigString("madokami.auth", null);
+        if (API_KEY == null) {
+            throw new IOException("Could not find madokami authentication key (madokami.auth) in configuration in the form of user:password");
+        }
+        headers = new HashMap<String, String>();
+        String encoded = Base64.encode(API_KEY.getBytes());
+        headers.put("Authorization", "Basic " + encoded);
+    }
+
+    @Override
+    public String getHost() {
+        return "madokami";
+    }
+
+    @Override
+    public String getDomain() {
+        return "manga.madokami.al";
+    }
+
+    @Override
+    public String getGID(URL url) throws MalformedURLException {
+        Pattern p = Pattern.compile("^https://manga\\.madokami\\.al/Manga/./.*/.*/(.*)$");
+        Matcher m = p.matcher(url.toExternalForm());
+        if (m.matches()) {
+            return m.group(1);
+        }
+        throw new MalformedURLException("Expected madokami URL format: " +
+                        "https://manga.madokami.al/Manga/./.*/.*/albumid - got " + url + " instead");
+    }
+
+    @Override
+    public Document getFirstPage() throws IOException {
+        return Http.url(url).get();
+    }
+
+    @Override
+    public List<String> getURLsFromPage(Document doc) {
+        List<String> fileURLs = new ArrayList<>();
+        for (Element link : doc.select("#index-table > tbody > tr > td:nth-child(1) > a")) {
+            String href = link.attr("href");
+            if(href.endsWith(".zip") || href.endsWith(".rar") || href.endsWith(".cbr") || href.endsWith(".cbz"))
+                fileURLs.add(href);
+        }
+        return fileURLs;
+    }
+
+    @Override
+    public void downloadURL(URL url, int index) {
+        File saveFile = getSaveFile(getPrefix(index), "", "", "");
+        addURLToDownload(url, saveFile, null, null, false, headers);
+    }
+}
