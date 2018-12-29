8b90e15c61ade4bdc4f3336f2c1e8d6990b35c4d
cyian-1756 <devnull64@vfemail.net>
2018-12-29 06:34:18 -0500

    Mark hentaiImage test as flaky

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/HentaiimageRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/HentaiimageRipperTest.java
@@ -4,10 +4,15 @@ import java.io.IOException;
 import java.net.URL;
 
 import com.rarchives.ripme.ripper.rippers.HentaiimageRipper;
+import com.rarchives.ripme.utils.Utils;
 
 public class HentaiimageRipperTest extends RippersTest {
+
     public void testHentaifoundryRip() throws IOException {
-        HentaiimageRipper ripper = new HentaiimageRipper(new URL("https://hentai-image.com/image/afrobull-gerudo-ongoing-12/"));
-        testRipper(ripper);
+        if (Utils.getConfigBoolean("test.run_flaky_tests", false)) {
+            HentaiimageRipper ripper = new HentaiimageRipper(new URL("https://hentai-image.com/image/afrobull-gerudo-ongoing-12/"));
+            testRipper(ripper);
+        }
     }
 }
+
