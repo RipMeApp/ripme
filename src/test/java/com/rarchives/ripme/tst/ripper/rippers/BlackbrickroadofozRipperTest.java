df95109cd803c16019f8d0b569f5d3a5879fd12e
cyian-1756 <devnull64@vfemail.net>
2019-04-02 16:30:33 -0400

    Commented out failing test

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/BlackbrickroadofozRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/BlackbrickroadofozRipperTest.java
@@ -6,8 +6,9 @@ import java.io.IOException;
 import java.net.URL;
 
 public class BlackbrickroadofozRipperTest extends RippersTest {
-    public void testRip() throws IOException {
-        BlackbrickroadofozRipper ripper = new BlackbrickroadofozRipper(new URL("http://www.blackbrickroadofoz.com/comic/beginning"));
-        testRipper(ripper);
-    }
+    // Commented out on 02/04/2019 because the serve has been down for a while
+//    public void testRip() throws IOException {
+//        BlackbrickroadofozRipper ripper = new BlackbrickroadofozRipper(new URL("http://www.blackbrickroadofoz.com/comic/beginning"));
+//        testRipper(ripper);
+//    }
 }
