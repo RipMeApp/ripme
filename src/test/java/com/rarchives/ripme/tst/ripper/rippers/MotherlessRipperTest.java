e0937378a2fda733a87cca0c2587319b47aebc56
pl77 <pl77@users.noreply.github.com>
2019-05-24 14:18:27 -0400

    Updated MotherlessRipperTest to point to a gallery that actually exists.
    Should pass Travis CI now...at least this test.

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/MotherlessRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/MotherlessRipperTest.java
@@ -8,7 +8,7 @@ import com.rarchives.ripme.ripper.rippers.MotherlessRipper;
 public class MotherlessRipperTest extends RippersTest {
     // https://github.com/RipMeApp/ripme/issues/238 - MotherlessRipperTest is flaky on Travis CI
     public void testMotherlessAlbumRip() throws IOException {
-        MotherlessRipper ripper = new MotherlessRipper(new URL("https://motherless.com/G1168D90"));
+        MotherlessRipper ripper = new MotherlessRipper(new URL("https://motherless.com/GIAB5CB01"));
         testRipper(ripper);
     }
 }
