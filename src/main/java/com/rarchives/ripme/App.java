8ced9b9ca4fb1598a687764dae67cd584fe809cd
inthebrilliantblue <intheprettypink@gmail.com>
2019-07-02 13:47:49 -0500

    Update App.java

--- src/main/java/com/rarchives/ripme/App.java
+++ src/main/java/com/rarchives/ripme/App.java
@@ -117,7 +117,7 @@ public class App {
         logger.info("Initialized ripme v" + UpdateUtils.getThisJarVersion());
         
         if (cl.hasOption('z')) {
-            String historyLocation = cl.getOption('z');
+            String historyLocation = cl.getOption('z').trim();
             Utils.setConfigString("history.location", historyLocation);
             logger.info("Set history file to " + historyLocation);
         }
