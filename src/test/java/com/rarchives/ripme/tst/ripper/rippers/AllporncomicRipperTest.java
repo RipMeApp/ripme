f70bca1f01c0f3ef136597bbad7e87d6933e32b7
cyian-1756 <devnull64@vfemail.net>
2019-08-04 11:57:02 -0400

    Removed copied paste test

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/AllporncomicRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/AllporncomicRipperTest.java
@@ -13,10 +13,4 @@ public class AllporncomicRipperTest extends RippersTest {
         testRipper(ripper);
     }
 
-    @Test
-    public void testGetGID() throws IOException {
-        URL url = new URL("https://allporncomic.com/porncomic/dnd-pvp-dungeons-dragons-fred-perry/1-dnd-pvp");
-        AllporncomicRipper ripper = new AllporncomicRipper(url);
-        assertEquals("5230", ripper.getGID(url));
-    }
 }
