a2fe593e39f56e51ea2f6108f0209eafb277a79e
cyian-1756 <devnull64@vfemail.net>
2019-08-13 20:57:45 -0400

    Reenabled accidently disabled tests

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/YoupornRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/YoupornRipperTest.java
@@ -1,6 +1,7 @@
 package com.rarchives.ripme.tst.ripper.rippers;
 
 import com.rarchives.ripme.ripper.rippers.YoupornRipper;
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.URL;
@@ -8,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class YoupornRipperTest  extends RippersTest {
+    @Test
     public void testYoupornRipper() throws IOException {
         List<URL> contentURLs = new ArrayList<>();
         contentURLs.add(new URL("http://www.youporn.com/watch/7669155/mrs-li-amateur-69-orgasm/?from=categ"));
