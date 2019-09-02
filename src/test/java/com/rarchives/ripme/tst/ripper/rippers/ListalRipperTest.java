9cd2c1f984e610f54d77aa623a4077515a212120
Tushar <tush_r67@hotmail.com>
2019-09-02 12:08:01 +0530

    Added support for folder type urls.

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/ListalRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/ListalRipperTest.java
@@ -6,9 +6,23 @@ import com.rarchives.ripme.ripper.rippers.ListalRipper;
 
 public class ListalRipperTest extends RippersTest {
 
-    public void testRip() throws IOException {
+    /**
+     * Test for list type url.
+     * @throws IOException
+     */
+    public void testRipListType() throws IOException {
         ListalRipper ripper =
                 new ListalRipper(new URL("https://www.listal.com/list/evolution-emma-stone"));
+        testRipper(ripper);
+    }
+
+    /**
+     * Test for folder type url.
+     * @throws IOException
+     */
+    public void testRipFolderType() throws IOException {
+        ListalRipper ripper =
+                new ListalRipper(new URL("https://www.listal.com/chet-atkins/pictures"));
         testRipper(ripper);
     }
 
