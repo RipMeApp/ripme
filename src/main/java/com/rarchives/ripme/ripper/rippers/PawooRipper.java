69befab7e5489f9f052906a58db9681eb9601fa5
0x1f595 <0x1f595@users.noreply.github.com>
2019-10-08 21:20:00 -0600

    Adding several Mastodon instance rippers

---
+++ src/main/java/com/rarchives/ripme/ripper/rippers/PawooRipper.java
@@ -0,0 +1,20 @@
+package com.rarchives.ripme.ripper.rippers;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class PawooRipper extends MastodonRipper {
+    public PawooRipper(URL url) throws IOException {
+        super(url);
+    }
+
+    @Override
+    public String getHost() {
+        return "pawoo";
+    }
+
+    @Override
+    public String getDomain() {
+        return "pawoo.net";
+    }
+}
