d83a9e0612605921624d1c19474fc0b29b4cfae4
0x1f595 <0x1f595@users.noreply.github.com>
2020-04-24 18:35:06 -0600

    Adding test case for new Mangadex URL form

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/MangadexRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/MangadexRipperTest.java
@@ -1,15 +1,22 @@
 package com.rarchives.ripme.tst.ripper.rippers;
 
-
 import com.rarchives.ripme.ripper.rippers.MangadexRipper;
+
+import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 import java.net.URL;
 
 public class MangadexRipperTest extends RippersTest{
+    @Test
     public void testRip() throws IOException {
         MangadexRipper ripper = new MangadexRipper(new URL("https://mangadex.org/chapter/467904/"));
         testRipper(ripper);
     }
 
+    @Test
+    public void testAltUrlRip() throws IOException {
+        MangadexRipper ripper = new MangadexRipper(new URL("https://www.mangadex.org/chapter/804465/1"));
+        testRipper(ripper);
+    }
 }
