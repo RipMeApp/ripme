ca1c78c4912a016a133e7a424086cfa630ac653d
soloturn <soloturn@gmail.com>
2021-03-28 08:35:55 +0200

    AerisdiesRipperTest flaky

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/AerisdiesRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/AerisdiesRipperTest.java
@@ -6,16 +6,19 @@ import java.net.URL;
 import com.rarchives.ripme.ripper.rippers.AerisdiesRipper;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class AerisdiesRipperTest extends RippersTest {
     @Test
+    @Tag("flaky")
     public void testAlbum() throws IOException {
         AerisdiesRipper ripper = new AerisdiesRipper(new URL("http://www.aerisdies.com/html/lb/alb_1097_1.html"));
         testRipper(ripper);
     }
 
     @Test
+    @Tag("flaky")
     public void testSubAlbum() throws IOException {
         AerisdiesRipper ripper = new AerisdiesRipper(new URL("http://www.aerisdies.com/html/lb/alb_3692_1.html"));
         testRipper(ripper);
