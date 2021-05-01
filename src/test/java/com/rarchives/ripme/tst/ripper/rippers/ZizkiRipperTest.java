7dc742dfcc5fc6a0b00e9cf7ba39ebd26ecd0f3d
soloturn <soloturn@gmail.com>
2021-05-01 08:20:27 +0200

    zizkiripper either broken or flaky

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/ZizkiRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/ZizkiRipperTest.java
@@ -5,20 +5,27 @@ import java.net.URL;
 
 import com.rarchives.ripme.ripper.rippers.ZizkiRipper;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class ZizkiRipperTest extends RippersTest {
+
+    @Test
+    @Tag("flaky")
     public void testRip() throws IOException {
         ZizkiRipper ripper = new ZizkiRipper(new URL("http://zizki.com/dee-chorde/we-got-spirit"));
         testRipper(ripper);
     }
 
+    @Test
     public void testGetGID() throws IOException {
         URL url = new URL("http://zizki.com/dee-chorde/we-got-spirit");
         ZizkiRipper ripper = new ZizkiRipper(url);
         Assertions.assertEquals("dee-chorde", ripper.getGID(url));
     }
+
     @Test
+    @Tag("flaky")
     public void testAlbumTitle() throws IOException {
         URL url = new URL("http://zizki.com/dee-chorde/we-got-spirit");
         ZizkiRipper ripper = new ZizkiRipper(url);
