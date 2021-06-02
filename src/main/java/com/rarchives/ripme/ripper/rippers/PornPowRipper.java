1a665ab2e0e9692ea396618521d015547d3367bc
prn39p <58423885+prn39p@users.noreply.github.com>
2021-06-02 16:44:39 +0530

    Adding Rippers
    Added Candidteens,Nudismlif and PornPow rippers

---
+++ src/main/java/com/rarchives/ripme/ripper/rippers/PornPowRipper.java
@@ -0,0 +1,101 @@
+
+
+package com.rarchives.ripme.ripper.rippers;
+
+import com.rarchives.ripme.ripper.AbstractHTMLRipper;
+import com.rarchives.ripme.ripper.AbstractRipper;
+import com.rarchives.ripme.utils.Http;
+import com.rarchives.ripme.utils.Utils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
+public class PornPowRipper extends AbstractHTMLRipper {
+
+    private static final String DOMAIN = "pornpaw.com", HOST = "pornpaw";
+
+    public PornPowRipper(URL url) throws IOException {
+        super(url);
+    }
+
+    @Override
+    public String getHost() {
+        return HOST;
+    }
+
+    @Override
+    public String getDomain() {
+        return DOMAIN;
+    }
+
+    @Override
+    public String getGID(URL url) throws MalformedURLException {
+
+        try {
+            Document doc = Jsoup.connect(url.toString())
+                    .userAgent(AbstractRipper.USER_AGENT)
+                    .get();
+            String title = doc.title();
+            title = title.replace(" porn pics", "");
+            return title;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "missed";
+        }
+    }
+
+    @Override
+    public Document getFirstPage() throws IOException {
+        return Http.url(url).get();
+    }
+
+    @Override
+    public List<String> getURLsFromPage(Document doc) {
+
+        List<String> result = new ArrayList<>();
+
+        for (Element el : doc.select("img.img")) {
+            String url =  el.attr("src");
+            result.add(url);
+        }
+        return result;
+
+    }
+
+    @Override
+    public void downloadURL(URL url, int index) {
+        addURLToDownload(url, getPrefix(index));
+    }
+
+    @Override
+    public void setWorkingDir(URL url) throws IOException {
+        String path = Utils.getWorkingDirectory().getCanonicalPath();
+        if (!path.endsWith(File.separator)) {
+            path += File.separator;
+        }
+        String title;
+        if (Utils.getConfigBoolean("album_titles.save", true)) {
+            title = getAlbumTitle(this.url);
+        } else {
+            title = super.getAlbumTitle(this.url);
+        }
+        LOGGER.debug("Using album title '" + title + "'");
+
+        path += title;
+        path = Utils.getOriginalDirectory(path) + File.separator;   // check for case sensitive (unix only)
+
+        this.workingDir = new File(path);
+        if (!this.workingDir.exists()) {
+            LOGGER.info("[+] Creating directory: " + Utils.removeCWD(this.workingDir));
+            this.workingDir.mkdirs();
+        }
+        LOGGER.debug("Set working directory to: " + this.workingDir);
+    }
+}
