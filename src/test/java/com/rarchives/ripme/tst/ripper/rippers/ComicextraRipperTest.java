7bef7d3af390d156e0cade84041688128864d39d
soloturn <soloturn@gmail.com>
2021-10-04 15:55:09 +0200

    comicextraripper flaky tests

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/ComicextraRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/ComicextraRipperTest.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 import java.net.URL;
 import com.rarchives.ripme.ripper.rippers.ComicextraRipper;
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class ComicextraRipperTest extends RippersTest {
     @Test
+    @Tag("flaky")
     public void testComicUrl() throws IOException {
         URL url = new URL("https://www.comicextra.com/comic/karma-police");
         ComicextraRipper ripper = new ComicextraRipper(url);
