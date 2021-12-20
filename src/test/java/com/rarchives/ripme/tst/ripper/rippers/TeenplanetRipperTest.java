eeae0ca8dd46504c775694bab94b21f56e5b5237
soloturn <soloturn@gmail.com>
2021-12-20 03:24:36 +0100

    mastodon, teenplane tests flaky

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/TeenplanetRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/TeenplanetRipperTest.java
@@ -5,10 +5,12 @@ import java.net.URL;
 
 import com.rarchives.ripme.ripper.rippers.TeenplanetRipper;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class TeenplanetRipperTest extends RippersTest {
     @Test
+    @Tag("flaky")
     public void testTeenplanetRip() throws IOException {
         TeenplanetRipper ripper = new TeenplanetRipper(new URL("http://teenplanet.org/galleries/the-perfect-side-of-me-6588.html"));
         testRipper(ripper);
