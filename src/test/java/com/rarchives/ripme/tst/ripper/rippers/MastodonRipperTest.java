f1db4300c2a8f81737bdc17de7ba7ecabf1cb5b2
soloturn <soloturn@gmail.com>
2021-12-20 03:24:36 +0100

    mastodon, teenplane tests flaky

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/MastodonRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/MastodonRipperTest.java
@@ -4,10 +4,12 @@ import java.io.IOException;
 import java.net.URL;
 
 import com.rarchives.ripme.ripper.rippers.MastodonRipper;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class MastodonRipperTest extends RippersTest {
     @Test
+    @Tag("flaky")
     public void testRip() throws IOException {
         MastodonRipper ripper = new MastodonRipper(new URL("https://mastodon.social/@pythonhub/media"));
         testRipper(ripper);
