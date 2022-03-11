b4ee50d5d1096ff7e6ca2d2835527e6224978425
soloturn <soloturn@gmail.com>
2022-03-11 04:55:11 +0100

    MyhentaicomicsRipperTest flaky

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/MyhentaicomicsRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/MyhentaicomicsRipperTest.java
@@ -5,10 +5,12 @@ import java.net.URL;
 
 import com.rarchives.ripme.ripper.rippers.MyhentaicomicsRipper;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class MyhentaicomicsRipperTest extends RippersTest {
     @Test
+    @Tag("flaky")
     public void testMyhentaicomicsAlbum() throws IOException {
         MyhentaicomicsRipper ripper = new MyhentaicomicsRipper(new URL("http://myhentaicomics.com/index.php/Nienna-Lost-Tales"));
         testRipper(ripper);
@@ -25,6 +27,7 @@ public class MyhentaicomicsRipperTest extends RippersTest {
         Assertions.assertEquals("2409", ripper.getGID(new URL("http://myhentaicomics.com/index.php/tag/2409/")));
     }
     @Test
+    @Tag("flaky")
     public void testGetAlbumsToQueue() throws IOException {
         URL url = new URL("https://myhentaicomics.com/index.php/tag/3167/");
         MyhentaicomicsRipper ripper = new MyhentaicomicsRipper(url);
