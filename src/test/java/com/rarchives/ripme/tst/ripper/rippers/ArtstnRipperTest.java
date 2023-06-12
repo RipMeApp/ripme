8cbdb33f04e8c3905dd90601c10d83916ee1c5da
soloturn <soloturn@gmail.com>
2023-06-12 19:42:52 +0200

    crfl --> cr, arttnruppertest

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/ArtstnRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/ArtstnRipperTest.java
@@ -1,6 +1,8 @@
 package com.rarchives.ripme.tst.ripper.rippers;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 import com.rarchives.ripme.ripper.rippers.ArtstnRipper;
@@ -12,15 +14,15 @@ import org.junit.jupiter.api.Test;
 public class ArtstnRipperTest extends RippersTest {
 	@Test
 	@Tag("flaky")
-	public void testSingleProject() throws IOException {
-		URL url = new URL("https://artstn.co/p/JlE15Z");
+	public void testSingleProject() throws IOException, URISyntaxException {
+		URL url = new URI("https://artstn.co/p/JlE15Z").toURL();
 		testRipper(new ArtstnRipper(url));
 	}
 
 	@Test
 	@Disabled("Failed with cloudflare protection")
-	public void testUserPortfolio() throws IOException {
-		URL url = new URL("https://artstn.co/m/rv37");
+	public void testUserPortfolio() throws IOException, URISyntaxException {
+		URL url = new URI("https://artstn.co/m/rv37").toURL();
 		testRipper(new ArtstnRipper(url));
 	}
 }
