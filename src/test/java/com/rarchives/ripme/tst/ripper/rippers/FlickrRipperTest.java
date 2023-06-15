5aeaf7d2b04be4d0a28a2fbd53af76b0be872736
soloturn <soloturn@gmail.com>
2023-06-15 14:30:12 +0200

    flickr test slow.

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/FlickrRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/FlickrRipperTest.java
@@ -7,11 +7,12 @@ import java.net.URISyntaxException;
 import com.rarchives.ripme.ripper.rippers.FlickrRipper;
 
 import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class FlickrRipperTest extends RippersTest {
     @Test
-    @Disabled("https://github.com/RipMeApp/ripme/issues/243")
+    @Tag("slow")
     public void testFlickrAlbum() throws IOException, URISyntaxException {
         FlickrRipper ripper = new FlickrRipper(
                 new URI("https://www.flickr.com/photos/leavingallbehind/sets/72157621895942720/").toURL());
