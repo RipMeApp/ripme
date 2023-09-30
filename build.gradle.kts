9c2404ec26e1b75cc01d6a109429e6ac76c79fd3
soloturn <soloturn@gmail.com>
2023-09-30 16:47:11 +0200

    gradle-8.3 deprecations fixed

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,8 +29,9 @@ dependencies {
   implementation("org.apache.logging.log4j:log4j-api:2.20.0")
   implementation("org.apache.logging.log4j:log4j-core:2.20.0")
   implementation("org.graalvm.js:js:22.3.2")
-  testImplementation(enforcedPlatform("org.junit:junit-bom:5.9.3"))
+  testImplementation(enforcedPlatform("org.junit:junit-bom:5.10.0"))
   testImplementation("org.junit.jupiter:junit-jupiter")
+  testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 group = "com.rarchives.ripme"
@@ -128,7 +129,7 @@ tasks.jacocoTestReport {
   reports {
     xml.required.set(false)
     csv.required.set(false)
-    html.outputLocation.set(file("${buildDir}/jacocoHtml"))
+    html.outputLocation.set(file("${layout.buildDirectory}/jacocoHtml"))
   }
 }
 
