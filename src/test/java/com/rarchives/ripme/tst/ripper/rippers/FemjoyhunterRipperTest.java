b080faaed86680f4f82eb91e19221f991963f1a9
soloturn <soloturn@gmail.com>
2023-12-01 16:57:10 +0100

    flaky test hentai, femjoy

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/FemjoyhunterRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/FemjoyhunterRipperTest.java
@@ -6,10 +6,12 @@ import java.net.URISyntaxException;
 
 import com.rarchives.ripme.ripper.rippers.FemjoyhunterRipper;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 public class FemjoyhunterRipperTest extends RippersTest {
     @Test
+    @Tag("flaky")
     public void testRip() throws IOException, URISyntaxException {
         FemjoyhunterRipper ripper = new FemjoyhunterRipper(new URI(
                 "https://www.femjoyhunter.com/alisa-i-got-nice-big-breasts-and-fine-ass-so-she-seems-to-be-a-hottest-brunette-5936/").toURL());
--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/HentaifoundryRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/HentaifoundryRipperTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 
 public class HentaifoundryRipperTest extends RippersTest {
     @Test
+    @Tag("flaky")
     public void testHentaifoundryRip() throws IOException, URISyntaxException {
         HentaifoundryRipper ripper = new HentaifoundryRipper(new URI("https://www.hentai-foundry.com/pictures/user/personalami").toURL());
         testRipper(ripper);
