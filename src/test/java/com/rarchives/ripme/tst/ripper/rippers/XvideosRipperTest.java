9b2ddec207aa7b84a7d78d3b172f82bc9cbaaf8b
MetaPrime <wheninthedark@gmail.com>
2025-01-02 04:38:57 -0800

    Updated XvideosRipperTest for new URL format

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/XvideosRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/XvideosRipperTest.java
@@ -9,9 +9,17 @@ import org.junit.jupiter.api.Test;
 
 public class XvideosRipperTest extends RippersTest {
     @Test
-    public void testXhamsterAlbum1() throws IOException, URISyntaxException {
-        XvideosRipper ripper = new XvideosRipper(new URI("https://www.xvideos.com/video23515878/dee_s_pool_toys").toURL());
+    public void testXvideosVideo1() throws IOException, URISyntaxException {
+        // This format is obsolete
+        // XvideosRipper ripper = new XvideosRipper(new URI("https://www.xvideos.com/video23515878/dee_s_pool_toys").toURL());
+        // The website now redirects that video to this page
+        XvideosRipper ripper = new XvideosRipper(new URI("https://www.xvideos.com/video.hppdiepcbfe/dee_s_pool_toys").toURL());
         testRipper(ripper);
     }
 
+    @Test
+    public void testXvideosVideo2() throws IOException, URISyntaxException {
+        XvideosRipper ripper = new XvideosRipper(new URI("https://www.xvideos.com/video.ufkmptkc4ae/big_tit_step_sis_made_me_cum_inside_her").toURL());
+        testRipper(ripper);
+    }
 }
