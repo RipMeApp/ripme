e9bd8fbb4545922792ee6f115c4ecfdc88b3d5e9
MetaPrime <wheninthedark@gmail.com>
2025-01-06 23:19:36 -0800

    PornhubRipperTest - update links to albums that aren't 404

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/PornhubRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/PornhubRipperTest.java
@@ -5,19 +5,28 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 
-import com.rarchives.ripme.ripper.rippers.PornhubRipper;
-import com.rarchives.ripme.utils.Http;
-import com.rarchives.ripme.utils.Utils;
 import org.jsoup.nodes.Document;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
+import com.rarchives.ripme.ripper.rippers.PornhubRipper;
+import com.rarchives.ripme.utils.Http;
+import com.rarchives.ripme.utils.Utils;
+
 public class PornhubRipperTest extends RippersTest {
     @Test
-    public void testPornhubRip() throws IOException, URISyntaxException {
+    public void testPornhubAlbumRip() throws IOException, URISyntaxException {
         if (Utils.getConfigBoolean("test.run_flaky_tests", false)) {
-            PornhubRipper ripper = new PornhubRipper(new URI("https://www.pornhub.com/album/15680522").toURL());
+            PornhubRipper ripper = new PornhubRipper(new URI("https://www.pornhub.com/album/6299702").toURL());
+            testRipper(ripper);
+        }
+    }
+
+    @Test
+    public void testPornhubMultiPageAlbumRip() throws IOException, URISyntaxException {
+        if (Utils.getConfigBoolean("test.run_flaky_tests", false)) {
+            PornhubRipper ripper = new PornhubRipper(new URI("https://www.pornhub.com/album/39341891").toURL());
             testRipper(ripper);
         }
     }
@@ -33,7 +42,7 @@ public class PornhubRipperTest extends RippersTest {
     @Test
     @Tag("flaky")
     public void testGetNextPage() throws IOException, URISyntaxException {
-        String baseURL = "https://www.pornhub.com/album/30687901";
+        String baseURL = "https://www.pornhub.com/album/39341891";
         PornhubRipper ripper = new PornhubRipper(new URI(baseURL).toURL());
         Document page = Http.url(baseURL).get();
         int numPagesRemaining = 1;
