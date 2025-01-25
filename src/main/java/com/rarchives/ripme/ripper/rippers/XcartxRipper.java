67ea64ca1bd9ad2af200d8fd5df68eb1a62d7133
Sayanjit Mukherjee <sayanjitmukherjee123@gmail.com>
2025-01-26 00:44:53 +0530

    Removed 'nexturl' variable , and removed unnecessary imports(unused ones)

--- src/main/java/com/rarchives/ripme/ripper/rippers/XcartxRipper.java
+++ src/main/java/com/rarchives/ripme/ripper/rippers/XcartxRipper.java
@@ -1,7 +1,6 @@
 package com.rarchives.ripme.ripper.rippers;
 
 import com.rarchives.ripme.ripper.AbstractHTMLRipper;
-import com.rarchives.ripme.utils.Http;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
