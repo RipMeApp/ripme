8f58eb41eabf7663932de08d5aefc6367db4cae6
MetaPrime <wheninthedark@gmail.com>
2025-02-02 03:05:02 -0800

    Chevereto tests flaky

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/CheveretoRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/CheveretoRipperTest.java
@@ -4,18 +4,21 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import com.rarchives.ripme.ripper.rippers.CheveretoRipper;
 
 public class CheveretoRipperTest extends RippersTest {
     @Test
+    @Tag("flaky")
     public void testSubdirAlbum1() throws IOException, URISyntaxException {
         CheveretoRipper ripper = new CheveretoRipper(new URI("https://kenzato.uk/booru/album/TnEc").toURL());
         testRipper(ripper);
     }
 
     @Test
+    @Tag("flaky")
     public void testSubdirAlbum2() throws IOException, URISyntaxException {
         CheveretoRipper ripper = new CheveretoRipper(new URI("https://kenzato.uk/booru/album/XWdIp").toURL());
         testRipper(ripper);
