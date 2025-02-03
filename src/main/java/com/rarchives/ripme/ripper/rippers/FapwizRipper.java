9b4675719000d6669dba6862fd6f9050f3e7b9c7
MetaPrime <wheninthedark@gmail.com>
2025-02-02 22:53:48 -0800

    Add FapwizRipperTest, fix bugs, add getNextPage

--- src/main/java/com/rarchives/ripme/ripper/rippers/FapwizRipper.java
+++ src/main/java/com/rarchives/ripme/ripper/rippers/FapwizRipper.java
@@ -8,26 +8,26 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import org.jsoup.nodes.Document;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
 import com.rarchives.ripme.ripper.AbstractHTMLRipper;
+import com.rarchives.ripme.utils.Http;
 
 public class FapwizRipper extends AbstractHTMLRipper {
 
     private static final Logger logger = LogManager.getLogger(FapwizRipper.class);
 
-    private static final Pattern CATEGORY_PATTERN =
-        Pattern.compile("https?://fapwiz.com/category/([a-zA-Z1-9_-]+)/?$");
+    private static final Pattern CATEGORY_PATTERN = Pattern.compile("https?://fapwiz.com/category/([a-zA-Z0-9_-]+)/?$");
 
-    private static final Pattern USER_PATTERN =
-        Pattern.compile("https?://fapwiz.com/([a-zA-Z1-9_-]+)/?$");
+    private static final Pattern USER_PATTERN = Pattern.compile("https?://fapwiz.com/([a-zA-Z0-9_-]+)/?$");
 
-    private static final Pattern POST_PATTERN =
-        Pattern.compile("https?://fapwiz.com/([a-zA-Z1-9_-]+)/([a-zA-Z1-9_-]+)/?$");
+    // Note that the last part of the pattern can contain unicode emoji which
+    // get encoded as %-encoded UTF-8 bytes in the URL, so we allow % characters.
+    private static final Pattern POST_PATTERN = Pattern
+            .compile("https?://fapwiz.com/([a-zA-Z0-9_-]+)/([a-zA-Z0-9_%-]+)/?$");
 
     public FapwizRipper(URL url) throws IOException {
         super(url);
@@ -59,49 +59,91 @@ public class FapwizRipper extends AbstractHTMLRipper {
 
         m = POST_PATTERN.matcher(url.toExternalForm());
         if (m.matches()) {
-            return m.group(1) + "_" + m.group(2);
+            return "post_" + m.group(1) + "_" + m.group(2);
         }
 
         throw new MalformedURLException("Expected fapwiz URL format: " +
-                "fapwiz.com/NAME - got " + url + " instead");
+                "fapwiz.com/USER or fapwiz.com/USER/POST or " +
+                "fapwiz.com/CATEGORY - got " + url + " instead");
+    }
+
+    void processUserOrCategoryPage(Document doc, List<String> results) {
+        // The category page looks a lot like the structure of a user page,
+        // so processUserPage is written to be compatible with both.
+        doc.select(".post-items-holder img").forEach(e -> {
+            String imgSrc = e.attr("src");
+
+            // Skip the user profile picture thumbnail insets
+            if (imgSrc.endsWith("-thumbnail-icon.jpg")) {
+                return;
+            }
+
+            // Replace -thumbnail.jpg with .mp4
+            String videoSrc = imgSrc.replace("-thumbnail.jpg", ".mp4");
+            results.add(videoSrc);
+        });
+    }
+
+    void processCategoryPage(Document doc, List<String> results) {
+        logger.info("Processing category page: " + url);
+        processUserOrCategoryPage(doc, results);
+    }
+
+    void processUserPage(Document doc, List<String> results) {
+        logger.info("Processing user page: " + url);
+        processUserOrCategoryPage(doc, results);
+    }
+
+    void processPostPage(Document doc, List<String> results) {
+        logger.info("Processing post page: " + url);
+        doc.select("video source").forEach(video -> {
+            results.add(video.attr("src"));
+        });
     }
 
     @Override
     public List<String> getURLsFromPage(Document doc) {
-        List<String> result = new ArrayList<>();
-
+        List<String> results = new ArrayList<>();
         Matcher m;
 
         m = CATEGORY_PATTERN.matcher(url.toExternalForm());
         if (m.matches()) {
-            // structure of user page - does it work for category pages too?
-            doc.select(".post-items-holder img").forEach(e -> {
-                String imgSrc = e.attr("src");
-                // Replace -thumbnail.jpg with .mp4
-                String videoSrc = imgSrc.replace("-thumbnail.jpg", ".mp4");
-                result.add(videoSrc);
-            });
+            processCategoryPage(doc, results);
         }
 
         m = USER_PATTERN.matcher(url.toExternalForm());
         if (m.matches()) {
-            // structure of user page
-            doc.select(".post-items-holder img").forEach(e -> {
-                String imgSrc = e.attr("src");
-                // Replace -thumbnail.jpg with .mp4
-                String videoSrc = imgSrc.replace("-thumbnail.jpg", ".mp4");
-                result.add(videoSrc);
-            });
+            processUserPage(doc, results);
         }
 
         m = POST_PATTERN.matcher(url.toExternalForm());
         if (m.matches()) {
-            doc.select("video source").forEach(video -> {
-                result.add(video.attr("src"));
-            });
+            processPostPage(doc, results);
         }
 
-        return result;
+        return results;
+    }
+
+    private Document getDocument(String url, int retries) throws IOException {
+        return Http.url(url).userAgent(USER_AGENT).retries(retries).get();
+    }
+
+    private Document getDocument(String url) throws IOException {
+        return getDocument(url, 1);
+    }
+
+    @Override
+    public Document getNextPage(Document page) throws IOException {
+        logger.info("Getting next page for url: " + url);
+        Elements next = page.select("a.next");
+        if (!next.isEmpty()) {
+            String href = next.attr("href");
+            logger.info("Found next page: " + href);
+            return getDocument(href);
+        } else {
+            logger.info("No more pages");
+            throw new IOException("No more pages.");
+        }
     }
 
     @Override
