c8db3e05a0cd9bb8e090ac007c4c5504ce18a6ae
MetaPrime <wheninthedark@gmail.com>
2025-04-20 00:37:12 -0700

    Fix ModelmayhemRipperTest

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/ModelmayhemRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/ModelmayhemRipperTest.java
@@ -4,26 +4,24 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import com.rarchives.ripme.ripper.rippers.ModelmayhemRipper;
-
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
-public class ModelmayhemRipperTest extends RippersTest {
+import com.rarchives.ripme.ripper.rippers.ModelmayhemRipper;
 
+public class ModelmayhemRipperTest extends RippersTest {
     @Test
-    @Disabled("Broken ripper")
     public void testModelmayhemRip() throws IOException, URISyntaxException {
         ModelmayhemRipper ripper = new ModelmayhemRipper(
-                new URI("https://www.modelmayhem.com/portfolio/520206/viewall").toURL());
+                new URI("https://www.modelmayhem.com/portfolio/4829413/viewall").toURL());
         testRipper(ripper);
     }
 
     @Test
     public void testGetGID() throws IOException, URISyntaxException {
         ModelmayhemRipper ripper = new ModelmayhemRipper(
-                new URI("https://www.modelmayhem.com/portfolio/520206/viewall").toURL());
-        Assertions.assertEquals("520206", ripper.getGID(new URI("https://www.modelmayhem.com/portfolio/520206/viewall").toURL()));
+                new URI("https://www.modelmayhem.com/portfolio/4829413/viewall").toURL());
+        Assertions.assertEquals("4829413",
+                ripper.getGID(new URI("https://www.modelmayhem.com/portfolio/4829413/viewall").toURL()));
     }
 }
