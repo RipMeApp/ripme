69c903c3a628045f27ac3e86bebabb3fdf6fe46b
metaprime <metaprime@users.noreply.github.com>
2025-04-19 23:44:37 -0700

    Update HentaiimageRipper for new URL (Fixes #2122) (#2123)

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/HentaiimageRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/HentaiimageRipperTest.java
@@ -4,17 +4,15 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import com.rarchives.ripme.ripper.rippers.HentaiimageRipper;
-import com.rarchives.ripme.utils.Utils;
 import org.junit.jupiter.api.Test;
+
+import com.rarchives.ripme.ripper.rippers.HentaiimageRipper;
 
 public class HentaiimageRipperTest extends RippersTest {
     @Test
     public void testHentaifoundryRip() throws IOException, URISyntaxException {
-        if (Utils.getConfigBoolean("test.run_flaky_tests", false)) {
-            HentaiimageRipper ripper = new HentaiimageRipper(new URI("https://hentai-image.com/image/afrobull-gerudo-ongoing-12/").toURL());
-            testRipper(ripper);
-        }
+        HentaiimageRipper ripper = new HentaiimageRipper(
+                new URI("https://hentai-img-xxx.com/image/afrobull-gerudo-ongoing-12/").toURL());
+        testRipper(ripper);
     }
 }
-
