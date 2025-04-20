0501f64e0c59b0d2daaaacafb5764ff38af79625
MetaPrime <wheninthedark@gmail.com>
2025-04-19 23:23:40 -0700

    Update EromeRipperTest

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/EromeRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/EromeRipperTest.java
@@ -5,46 +5,61 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 
-import com.rarchives.ripme.ripper.rippers.EromeRipper;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-public class EromeRipperTest extends RippersTest {
-	@Test
-	public void testGetGIDProfilePage() throws IOException, URISyntaxException {
-		URL url = new URI("https://www.erome.com/Jay-Jenna").toURL();
-		EromeRipper ripper = new EromeRipper(url);
-		Assertions.assertEquals("Jay-Jenna", ripper.getGID(url));
-	}
-	@Test
-	public void testGetGIDAlbum() throws IOException, URISyntaxException {
-		URL url = new URI("https://www.erome.com/a/KbDAM1XT").toURL();
-		EromeRipper ripper = new EromeRipper(url);
-		Assertions.assertEquals("KbDAM1XT", ripper.getGID(url));
-	}
-	@Test
-	public void testGetAlbumsToQueue() throws IOException, URISyntaxException {
-		URL url = new URI("https://www.erome.com/Jay-Jenna").toURL();
-		EromeRipper ripper = new EromeRipper(url);
-		assert (2 >= ripper.getAlbumsToQueue(ripper.getFirstPage()).size());
-	}
-	@Test
-	public void testPageContainsAlbums() throws IOException, URISyntaxException {
-		URL url = new URI("https://www.erome.com/Jay-Jenna").toURL();
-		EromeRipper ripper = new EromeRipper(url);
-		assert (ripper.pageContainsAlbums(url));
-		assert (!ripper.pageContainsAlbums(new URI("https://www.erome.com/a/KbDAM1XT").toURL()));
-	}
+import com.rarchives.ripme.ripper.rippers.EromeRipper;
 
-	public void testRip() throws IOException, URISyntaxException {
-		URL url = new URI("https://www.erome.com/a/vlefBdsg").toURL();
-		EromeRipper ripper = new EromeRipper(url);
-		testRipper(ripper);
-	}
-	@Test
-	public void testGetURLsFromPage() throws IOException, URISyntaxException {
-		URL url = new URI("https://www.erome.com/a/Tak8F2h6").toURL();
-		EromeRipper ripper = new EromeRipper(url);
-		assert (35 == ripper.getURLsFromPage(ripper.getFirstPage()).size());
-	}
+public class EromeRipperTest extends RippersTest {
+    @Test
+    public void testGetGIDProfilePage() throws IOException, URISyntaxException {
+        URL url = new URI("https://www.erome.com/Jay-Jenna").toURL();
+        EromeRipper ripper = new EromeRipper(url);
+        Assertions.assertEquals("Jay-Jenna", ripper.getGID(url));
+    }
+
+    @Test
+    public void testGetGIDAlbum() throws IOException, URISyntaxException {
+        URL url = new URI("https://www.erome.com/a/KbDAM1XT").toURL();
+        EromeRipper ripper = new EromeRipper(url);
+        Assertions.assertEquals("KbDAM1XT", ripper.getGID(url));
+    }
+
+    @Test
+    public void testGetAlbumsToQueue() throws IOException, URISyntaxException {
+        URL url = new URI("https://www.erome.com/Jay-Jenna").toURL();
+        EromeRipper ripper = new EromeRipper(url);
+        assert (2 >= ripper.getAlbumsToQueue(ripper.getFirstPage()).size());
+    }
+
+    @Test
+    public void testPageContainsAlbums() throws IOException, URISyntaxException {
+        URL url = new URI("https://www.erome.com/Jay-Jenna").toURL();
+        EromeRipper ripper = new EromeRipper(url);
+        assert (ripper.pageContainsAlbums(url));
+        assert (!ripper.pageContainsAlbums(new URI("https://www.erome.com/a/KbDAM1XT").toURL()));
+    }
+
+    @Test
+    public void testRip() throws IOException, URISyntaxException {
+        URL url = new URI("https://www.erome.com/a/Tak8F2h6").toURL();
+        EromeRipper ripper = new EromeRipper(url);
+        testRipper(ripper);
+    }
+
+    @Test
+    @Tag("slow")
+    public void testVideoAlbumRip() throws IOException, URISyntaxException {
+        URL url = new URI("https://www.erome.com/a/P0x5Ambn").toURL();
+        EromeRipper ripper = new EromeRipper(url);
+        testRipper(ripper);
+    }
+
+    @Test
+    public void testGetURLsFromPage() throws IOException, URISyntaxException {
+        URL url = new URI("https://www.erome.com/a/Tak8F2h6").toURL();
+        EromeRipper ripper = new EromeRipper(url);
+        assert (35 == ripper.getURLsFromPage(ripper.getFirstPage()).size());
+    }
 }
