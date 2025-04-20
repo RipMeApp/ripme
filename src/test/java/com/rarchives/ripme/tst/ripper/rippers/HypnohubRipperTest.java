818e524c3bfa9eb1e1b9d2f2f630960537417bea
MetaPrime <wheninthedark@gmail.com>
2025-04-19 23:51:13 -0700

    Formatted HypnohubRipperTest

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/HypnohubRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/HypnohubRipperTest.java
@@ -5,29 +5,33 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 
-import com.rarchives.ripme.ripper.rippers.HypnohubRipper;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import com.rarchives.ripme.ripper.rippers.HypnohubRipper;
+
 public class HypnohubRipperTest extends RippersTest {
     @Test
-    @Disabled("wants a hunman")
+    @Disabled("wants a human")
     public void testRip() throws IOException, URISyntaxException {
         URL poolURL = new URI("http://hypnohub.net/pool/show/2303").toURL();
-        URL postURL = new URI("http://hypnohub.net/post/show/63464/black_hair-bracelet-collar-corruption-female_only-").toURL();
+        URL postURL = new URI("http://hypnohub.net/post/show/63464/black_hair-bracelet-collar-corruption-female_only-")
+                .toURL();
         HypnohubRipper ripper = new HypnohubRipper(poolURL);
         testRipper(ripper);
         ripper = new HypnohubRipper(postURL);
         testRipper(ripper);
     }
+
     @Test
     public void testGetGID() throws IOException, URISyntaxException {
         URL poolURL = new URI("http://hypnohub.net/pool/show/2303").toURL();
         HypnohubRipper ripper = new HypnohubRipper(poolURL);
         Assertions.assertEquals("2303", ripper.getGID(poolURL));
 
-        URL postURL = new URI("http://hypnohub.net/post/show/63464/black_hair-bracelet-collar-corruption-female_only-").toURL();
+        URL postURL = new URI("http://hypnohub.net/post/show/63464/black_hair-bracelet-collar-corruption-female_only-")
+                .toURL();
         Assertions.assertEquals("63464_black_hair-bracelet-collar-corruption-female_only-", ripper.getGID(postURL));
     }
 }
