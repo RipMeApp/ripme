46ac407c1450b67d8ede7948614e06b59889f55a
metaprime <metaprime@users.noreply.github.com>
2025-04-19 19:41:46 -0700

    Update CfakeRipper to new site format (Fixes #2116) (#2117)

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/CfakeRipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/CfakeRipperTest.java
@@ -4,11 +4,15 @@ import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 
+import org.junit.jupiter.api.Test;
+
 import com.rarchives.ripme.ripper.rippers.CfakeRipper;
 
 public class CfakeRipperTest extends RippersTest {
+    @Test
     public void testRip() throws IOException, URISyntaxException {
-        CfakeRipper ripper = new CfakeRipper(new URI("http://cfake.com/picture/Zooey_Deschanel/1264").toURL());
+        CfakeRipper ripper = new CfakeRipper(
+                new URI("https://cfake.com/images/celebrity/Zooey_Deschanel/1264").toURL());
         testRipper(ripper);
     }
 }
