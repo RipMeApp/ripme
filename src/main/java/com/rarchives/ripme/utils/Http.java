b9f6e02223a7146c32a08d2c7b1a093608dc3f4c
Laziassdev <tinglaziass@gmail.com>
2025-05-31 13:01:41 +0100

    fix missing import

--- src/main/java/com/rarchives/ripme/utils/Http.java
+++ src/main/java/com/rarchives/ripme/utils/Http.java
@@ -28,6 +28,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
+import java.util.Random;
 
 /**
  * Wrapper around the Jsoup connection methods.
