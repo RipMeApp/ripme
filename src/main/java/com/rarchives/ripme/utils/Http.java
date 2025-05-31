878257b1a696a7cf6f12ae7e4ce20b46f9fe4398
Laziassdev <tinglaziass@gmail.com>
2025-05-31 07:55:07 +0100

    missing imports

--- src/main/java/com/rarchives/ripme/utils/Http.java
+++ src/main/java/com/rarchives/ripme/utils/Http.java
@@ -23,6 +23,11 @@ import java.security.SecureRandom;
 import java.security.cert.X509Certificate;
 import java.util.HashMap;
 import java.util.Map;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
 
 /**
  * Wrapper around the Jsoup connection methods.
