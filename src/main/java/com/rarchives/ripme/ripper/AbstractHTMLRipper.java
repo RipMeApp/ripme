1b0f20085be593b8cef4cc6f66c7d50c3eef3c35
Laziassdev <tinglaziass@gmail.com>
2025-05-31 03:24:20 +0100

    new changes to handle 429

--- src/main/java/com/rarchives/ripme/ripper/AbstractHTMLRipper.java
+++ src/main/java/com/rarchives/ripme/ripper/AbstractHTMLRipper.java
@@ -22,6 +22,7 @@ import java.util.Map;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jsoup.nodes.Document;
+import org.jsoup.Jsoup;
 
 import com.rarchives.ripme.ui.MainWindow;
 import com.rarchives.ripme.ui.RipStatusMessage;
