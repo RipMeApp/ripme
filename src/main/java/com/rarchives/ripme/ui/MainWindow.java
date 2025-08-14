42afdccd89de91c435c1bfda3fcc57de2aee0cb2
iqqu <33103874+iqqu@users.noreply.github.com>
2025-08-14 11:57:26 -0400

    Open https URL

--- src/main/java/com/rarchives/ripme/ui/MainWindow.java
+++ src/main/java/com/rarchives/ripme/ui/MainWindow.java
@@ -1298,7 +1298,7 @@ public final class MainWindow implements Runnable, RipStatusHandler {
                         JOptionPane.YES_NO_OPTION, JOptionPane.PLAIN_MESSAGE, new ImageIcon(mainIcon));
                 if (response == JOptionPane.YES_OPTION) {
                     try {
-                        Utils.browse(URI.create("http://github.com/ripmeapp/ripme"));
+                        Utils.browse(URI.create("https://github.com/ripmeapp/ripme"));
                     } catch (IOException e) {
                         LOGGER.error("Exception while opening project home page", e);
                     }
