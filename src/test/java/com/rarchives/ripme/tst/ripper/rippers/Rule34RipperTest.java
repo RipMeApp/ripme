98e632c4754d2e8624bb36c515473b04076e8c5b
MetaPrime <wheninthedark@gmail.com>
2025-09-20 02:04:10 -0700

    Rule34RipperTest > disable flaky test testShesFreakyRip

--- src/test/java/com/rarchives/ripme/tst/ripper/rippers/Rule34RipperTest.java
+++ src/test/java/com/rarchives/ripme/tst/ripper/rippers/Rule34RipperTest.java
@@ -5,14 +5,18 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URL;
 
-import com.rarchives.ripme.ripper.rippers.Rule34Ripper;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+
+import com.rarchives.ripme.ripper.rippers.Rule34Ripper;
 
 public class Rule34RipperTest extends RippersTest {
     @Test
+    @Tag("flaky")
     public void testShesFreakyRip() throws IOException, URISyntaxException {
-        Rule34Ripper ripper = new Rule34Ripper(new URI("https://rule34.xxx/index.php?page=post&s=list&tags=bimbo").toURL());
+        Rule34Ripper ripper = new Rule34Ripper(
+                new URI("https://rule34.xxx/index.php?page=post&s=list&tags=bimbo").toURL());
         testRipper(ripper);
     }
 
